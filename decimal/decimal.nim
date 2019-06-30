19a0ef05f5bff461de8275e43f95956809450513
Hugo <hajenzoo29@gmail.com>
2019-06-30 10:56:42 +0200

    implement some of the high level api

--- decimal/decimal.nim
+++ decimal/decimal.nim
@@ -6,3 +6,93 @@
 # at your option. This file may not be copied, modified, or distributed except according to those terms.
 
 import decimal_lowlevel
+
+type 
+    DecimalType = ptr mpd_t
+
+const
+    DEFAULT_PREC = MPD_RDIGITS * 2
+    DEFAULT_EMAX = when (sizeof(int) == 8): 999999999999999999 else: 425000000
+    DEFAULT_EMIN = when (sizeof(int) == 8): -999999999999999999 else: -425000000
+
+var CTX: mpd_context_t
+var CTX_ADDR = addr CTX
+mpd_defaultcontext(CTX_ADDR)
+
+proc setPrec*(prec: int) =
+    if 0 < prec:
+        let success = mpd_qsetprec(CTX_ADDR, prec)
+        if success == 0:
+            echo "Bad prec" 
+
+proc `$`*(s: DecimalType): cstring = mpd_to_sci(s, 0)
+
+proc initDecimal*(): DecimalType = mpd_new(CTX_ADDR)
+
+proc newDecimal*(s: string): DecimalType =
+    result = mpd_new(CTX_ADDR)
+    mpd_set_string(result, s, CTX_ADDR)
+
+proc newDecimal*(s: int64): DecimalType =
+    result = mpd_new(CTX_ADDR)
+    mpd_set_i64(result, s, CTX_ADDR)
+
+proc newDecimal*(s: int32): DecimalType =
+    result = mpd_new(CTX_ADDR)
+    mpd_set_i32(result, s, CTX_ADDR)
+
+proc clone*(b: DecimalType): DecimalType =
+    result = initDecimal()
+    mpd_copy(result, b, CTX_ADDR)
+
+proc `+`*(a, b: DecimalType): DecimalType =
+    result = initDecimal()
+    mpd_add(result, a, b, CTX_ADDR)
+
+proc `+=`*(a, b: DecimalType) =
+    mpd_add(a, a, b, CTX_ADDR)
+
+proc `-`*(a, b: DecimalType): DecimalType =
+    result = initDecimal()
+    mpd_sub(result, a, b, CTX_ADDR)
+
+proc `-=`*(a, b: DecimalType) =
+    mpd_sub(a, a, b, CTX_ADDR)
+
+proc `*`*(a, b: DecimalType): DecimalType =
+    result = initDecimal()
+    mpd_mul(result, a, b, CTX_ADDR)
+
+proc `*=`*(a, b: DecimalType) =
+    mpd_mul(a, a, b, CTX_ADDR)
+
+proc `/`*(a, b: DecimalType): DecimalType =
+    result = initDecimal()
+    mpd_div(result, a, b, CTX_ADDR)
+
+proc `/=`*(a, b: DecimalType) =
+    mpd_div(a, a, b, CTX_ADDR)
+
+proc `^`*(a, b: DecimalType): DecimalType =
+    result = initDecimal()
+    mpd_pow(result, a, b, CTX_ADDR)
+
+proc divint*(a, b: DecimalType): DecimalType =
+    result = initDecimal()
+    mpd_divint(result, a, b, CTX_ADDR)
+
+proc rem*(a, b: DecimalType): DecimalType =
+    result = initDecimal()
+    mpd_rem(result, a, b, CTX_ADDR)
+
+proc divmod*(a, b: DecimalType): (DecimalType, DecimalType) =
+    let q = initDecimal()
+    let r = initDecimal()
+    mpd_divmod(q, r, a, b, CTX_ADDR)
+    result = (q, r)
+
+
+proc exp*(a: DecimalType): DecimalType =
+    result = initDecimal()
+    mpd_exp(result, a, CTX_ADDR)
+
